5308483ef664da3921861719cb45156346695974
RITHY Bondeth <rithybondeth999@gmail.com>
2026-07-05 22:12:32 +0700

    chore: add section headers to route path constants for better organization

--- lib/core/constants/route_path_contant.dart
+++ lib/core/constants/route_path_contant.dart
@@ -1,12 +1,18 @@
 class RoutePathConstant {
   RoutePathConstant._();
 
+  // ==================================================
+  // AUTH ROUTES
+  // ==================================================
   static String loginPath = '/login';
   static String forgotPasswordPath = '/forgot-password';
   static String resetPasswordPath = '/reset-passowrd';
   static String phoneNumberLoginPath = '/phone-number';
   static String phoneOTPPath = '/phone-otp';
 
+  // ==================================================
+  // MAIN ROUTES
+  // ==================================================
   static String homePath = '/home';
   static String feedPath = 'feed';
   static String searchPath = 'search';
